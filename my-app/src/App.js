4ad1ae3277825afa796581d48aca6f5bc2059f97
Errol Toney <liveingn21@gmail.com>
2021-12-31 09:39:09 -0500

    library imports
    info component

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Info from './components/Info';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className="App-header"> Toney</h1>
+   <Info/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Info.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Info.css";
+
+function Info() {
+  return (
+    <div>
+      <ol className="orderdList">
+        <li className= "nav-item" href="https://www.linkedin.com/in/errol-toney-1a98711bb/">Linkedin</li>
+        <li  className= "nav-item" href="https://github.com/ErrolTToney">GitHub</li>
+      </ol>
+    </div>
+  );
+}
+
+export default Info;
